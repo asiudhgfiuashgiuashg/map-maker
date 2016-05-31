d1f986c5272e0fcfe309eda7a02d68198c19eb84
Mike <mikeadkison@gmail.com>
2016-05-31 04:48:19 -0400

    put the canvasobject class in its own file

--- MapGUI.java
+++ MapGUI.java
@@ -90,223 +90,7 @@ public class MapGUI extends Application {
 
 	private double zoomPercent;
 
-	public static class CanvasObject extends Canvas {
-		protected static final List<CanvasObject> canvasObjects = new ArrayList<>();
-
-		// Unique Id
-		private static int uidIncrementer = 0;
-
-		// Canvas dimensions
-		protected Image objectImage; //image after scaling with zoom
-		protected double imgWidth; //image height after scaling with zoom
-		protected double imgHeight; //image width after scaling with zoom
-
-		// Object properties
-		public String fileName;
-		public double x; //the "real" position of the object aka its position at 100% zoom scaling
-		public double y;
-		public int visLayer;
-		public Boolean collision;
-		public String[] extraProps;
-
-		// Edit properties window dimensions
-		private final int propWidth = 400;
-		private final int propHeight = 250;
-
-		// Default property values
-		private final int tileVisLayer = 1;
-		private final int playerVisLayer = 5;
-		private final int defVisLayer = 9;
-		private final Boolean defCollision = true;
-
-		private double zoomPercent; // the percentage by which the map is zoomed in the map maker.
-									// we need this value to know how big to make the object's image
-
-		public CanvasObject(Image objectImage, double imgWidth, double imgHeight, String fileName, double x, double y, double zoomPercent) {
-			this.objectImage = objectImage;
-			this.imgWidth = imgWidth;
-			this.imgHeight = imgHeight;
-			this.fileName = fileName;
-			this.x = x;
-			this.y = y;
-			this.visLayer = defVisLayer;
-			this.collision = defCollision;
-			this.extraProps = null;
-			CreateObject();
-			setZoomPercent(zoomPercent);
-		}
-
-		public CanvasObject(Image objectImage, double imgWidth, double imgHeight, String fileName, double x, double y, double zoomPercent, int visLayer, Boolean collision, String[] extraProps) {
-			this.objectImage = objectImage;
-			this.imgWidth = imgWidth;
-			this.imgHeight = imgHeight;
-			this.fileName = fileName;
-			this.x = x;
-			this.y = y;
-			this.visLayer = visLayer;
-			this.collision = collision;
-			this.extraProps = extraProps;
-			CreateObject();
-			setZoomPercent(zoomPercent);
-		}
-
-		public void CreateObject() {
-			CanvasObject.canvasObjects.add(this);
-
-			// Set Unique Id
-			this.setId("object" + uidIncrementer);
-			uidIncrementer++;
-
-			// Add to Pane
-			tilePane.getChildren().add(CanvasObject.this);
-
-			// On drag, move object
-			this.setOnMouseDragged(new EventHandler<MouseEvent>() {
-				@Override
-				public void handle(MouseEvent event) {
-					if (event.getButton() == MouseButton.PRIMARY) {
-						// Update position
-						CanvasObject.this.x = fromScaled(event.getX() + CanvasObject.this.getTranslateX(), zoomPercent);
-						CanvasObject.this.y = fromScaled(event.getY() + CanvasObject.this.getTranslateY(), zoomPercent);
-						CanvasObject.this.setTranslateX(toScaled(CanvasObject.this.x, zoomPercent) - CanvasObject.this.imgWidth / 2);
-						CanvasObject.this.setTranslateY(toScaled(CanvasObject.this.y, zoomPercent) - CanvasObject.this.imgHeight / 2);
-					}
-				}
-			});
-
-			this.setOnMouseClicked(new EventHandler<MouseEvent>() {
-				@Override
-				public void handle(MouseEvent event) {
-					// On right click delete image
-					if (event.getButton() == MouseButton.SECONDARY) {
-						tilePane.getChildren().remove(CanvasObject.this);
-					}
-
-					// On double click edit properties
-					if (event.getButton() == MouseButton.PRIMARY) {
-						if (event.getClickCount() == 2) {
-							Dialog<String> editDialog = new Dialog<>();
-							editDialog.setTitle("Edit Object Properties");
-							editDialog.setHeaderText("Visibility Layers:\nInvisible = 0, Tiles = " + tileVisLayer + ", Player = " + playerVisLayer + ", Default = " + defVisLayer + "\n(Additional properties delimited by comma)");
-
-							ButtonType okButtonType = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
-							editDialog.getDialogPane().getButtonTypes().addAll(okButtonType, ButtonType.CANCEL);
-
-							Label layerLabel = new Label("Visibility Layer:");
-							Label collLabel = new Label("Collisions:");
-							Label additLabel = new Label("Additional Properties:");
-
-							// Visibility Layer
-							TextField layerTextField = new TextField(Integer.toString(visLayer));
-
-							// Make sure character is numeric
-							layerTextField.addEventFilter(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
-								@Override
-								public void handle(KeyEvent e) {
-									if (!Character.isDigit(e.getCharacter().charAt(0))) {
-										e.consume();
-									}
-								}
-							});
-
-							// Make sure layer is not player, tile layer, or null
-							layerTextField.addEventFilter(KeyEvent.KEY_RELEASED, new EventHandler<KeyEvent>() {
-								@Override
-								public void handle(KeyEvent e) {
-									Node okButton = editDialog.getDialogPane().lookupButton(okButtonType);
-									if (layerTextField.getText().equals(Integer.toString(playerVisLayer)) || layerTextField.getText().equals(Integer.toString(tileVisLayer)) || layerTextField.getText().equals("")) {
-										okButton.setDisable(true);
-									} else {
-										okButton.setDisable(false);
-									}
-								}
-							});
-
-							// Collision choice box
-							ChoiceBox<String> collChoiceBox = new ChoiceBox<String>();
-							collChoiceBox.getItems().addAll("true", "false");
-							if (collision) {
-								collChoiceBox.getSelectionModel().selectFirst();
-							} else {
-								collChoiceBox.getSelectionModel().selectLast();
-							}
-
-							// Addition properties textbox
-							TextField additTextField = new TextField();
-							if (CanvasObject.this.extraProps != null) {
-								additTextField.setText(String.join(",", CanvasObject.this.extraProps));
-							}
-
-							// Grid pane for dialog box
-							GridPane propGridPane = new GridPane();
-							propGridPane.setHgap(10);
-							propGridPane.setVgap(10);
-							propGridPane.add(layerLabel, 0, 0);
-							propGridPane.add(layerTextField, 1, 0);
-							propGridPane.add(collLabel, 0, 1);
-							propGridPane.add(collChoiceBox, 1, 1);
-							propGridPane.add(additLabel, 0, 2);
-							propGridPane.add(additTextField, 1, 2);
-
-							editDialog.setResultConverter(dialogButton -> {
-								if (dialogButton == okButtonType) {
-									// Make sure the text field isn't player, tile layer, or null
-									if (!(layerTextField.getText().equals(Integer.toString(playerVisLayer)) || layerTextField.getText().equals(Integer.toString(tileVisLayer)) || layerTextField.getText().equals(""))) {
-										// Update properties
-										CanvasObject.this.visLayer = Integer.parseInt(layerTextField.getText());
-										CanvasObject.this.collision = Boolean.parseBoolean(collChoiceBox.getSelectionModel().getSelectedItem());
-										CanvasObject.this.extraProps = additTextField.getText().split(",");
-									}
-								}
-								return null;
-							});
-
-							editDialog.getDialogPane().setContent(propGridPane);
-							editDialog.showAndWait();
-						}
-					}
-				}
-			});
-		}
-
-		/**
-		 * set the canvas which will contain object's image to be the same size as the image
-		 *  and then draw the image on the canvas
-		 */
-		private void drawImage() {
-			this.setWidth(imgWidth);
-			this.setHeight(imgHeight);
-			//clear the canvas for this object
-			this.getGraphicsContext2D().clearRect(0, 0, this.getWidth(), this.getHeight());
-
-			// Draw image onto the object's canvas
-			this.getGraphicsContext2D().drawImage(objectImage, 0, 0);
-			this.setTranslateX(toScaled(CanvasObject.this.x, zoomPercent) - CanvasObject.this.imgWidth / 2);
-			this.setTranslateY(toScaled(CanvasObject.this.y, zoomPercent) - CanvasObject.this.imgHeight / 2);
-		}
-
-		/**
-		 * zoom this canvasobject by some zoom percentage. This will scale the image and also translate it to the correct position
-		 */
-		protected void setZoomPercent(double newZoom) {
-			this.zoomPercent = newZoom;
-
-			ImageWithURL objImage = (ImageWithURL) objectImage;
-			String objImageURL = objImage.getURL();
-
-			//get the original dimensions of the object's image
-			Image unzoomedImage = new Image(objImageURL);
-			double unzoomedWidth = unzoomedImage.getWidth();
-			double unzoomedHeight = unzoomedImage.getHeight();
-
-			//now create a scaled (zoomed) version of the image to display as the object on the map
-			ImageWithURL zoomedImage = new ImageWithURL(objImageURL, zoomPercent / 100 * unzoomedWidth, zoomPercent / 100 * unzoomedHeight, true, false, false);
-			objectImage = zoomedImage;
-			imgWidth = zoomedImage.getWidth();
-			imgHeight = zoomedImage.getHeight();
-			drawImage();
-		}
-	}
+	
 
 	private void updateTileCanvasDimensions() {
 		tileCanvas.setWidth(tileSizeX * tileCols * (zoomPercent / 100));
@@ -337,7 +121,7 @@ public class MapGUI extends Application {
 							double imgWidth = objectImage.getWidth();
 							double imgHeight = objectImage.getHeight();
 							objectImage = new ImageWithURL("file:" + workToObjectAsset + relativeSelectedObject, zoomPercent / 100 * imgWidth, zoomPercent / 100 * imgHeight, true, false, false);
-							new CanvasObject(objectImage, imgWidth * (zoomPercent / 100), imgHeight * (zoomPercent / 100), relativeSelectedObject, fromScaled(event.getX(), zoomPercent), fromScaled(event.getY(), zoomPercent), zoomPercent);
+							tilePane.getChildren().add(new CanvasObject(objectImage, imgWidth * (zoomPercent / 100), imgHeight * (zoomPercent / 100), relativeSelectedObject, fromScaled(event.getX(), zoomPercent), fromScaled(event.getY(), zoomPercent), zoomPercent));
 						} catch (IllegalArgumentException e) {
 							System.out.println("object file not found or file out of map-maker directory");
 							System.exit(0);
@@ -730,8 +514,7 @@ public class MapGUI extends Application {
 								objectImage = new ImageWithURL("file:" + workToObjectAsset + fileName, false);
 								double imgWidth = objectImage.getWidth();
 								double imgHeight = objectImage.getHeight();
-								new CanvasObject(objectImage, imgWidth, imgHeight, fileName, x, y, zoomPercent, visLayer, collision, extraProps);
-								System.out.println("loaded an object at pos (" + x + ", " + y + ")");
+								tilePane.getChildren().add(new CanvasObject(objectImage, imgWidth, imgHeight, fileName, x, y, zoomPercent, visLayer, collision, extraProps));
 							} catch (IllegalArgumentException e) {
 								System.out.println("object file not found or file out of map-maker directory");
 								System.exit(0);
@@ -998,7 +781,7 @@ public class MapGUI extends Application {
 	 * convert a scaled coordinate to 100% scale
 	 * example: if the zoom percentage is 200%, this will return half of what you pass in
 	 */
-	private static double fromScaled(double scaledCoord, double zoomPercent) {
+	protected static double fromScaled(double scaledCoord, double zoomPercent) {
 		return scaledCoord / (zoomPercent / 100);
 	}
 
@@ -1007,7 +790,7 @@ public class MapGUI extends Application {
 	 * @param toScale what you want to scale
 	 * @param zoomPercent the percentage by which to scale the coordinate value
 	 */
-	private static double toScaled(double toScale, double zoomPercent) {
+	protected static double toScaled(double toScale, double zoomPercent) {
 		return toScale * (zoomPercent / 100);
 	}
 }
